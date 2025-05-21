3fd0b1fd285bfc61de9bc2d905a2844d6bf21f22
Harry <thcstrandainghiathinh6a1@gmail.com>
2025-05-22 01:19:01 +0700

    Prepare for Azure deployment

--- SWD-BloodDonation/Program.cs
+++ SWD-BloodDonation/Program.cs
@@ -1,14 +1,18 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using SWD_BloodDonation.Models;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Đăng ký services
+// Đăng ký DbContext và sử dụng connection string trong appsettings.json
+builder.Services.AddDbContext<BloodDonationContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 
+builder.Services.AddControllers();
 
 var app = builder.Build();
 
-// Map controllers
 app.MapControllers();
 
 app.Run();
