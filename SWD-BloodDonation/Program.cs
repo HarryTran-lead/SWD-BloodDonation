2e05722a5f7c5a1e8fde6d7e900a491c3cf10ea6
Harry <thcstrandainghiathinh6a1@gmail.com>
2025-05-22 00:51:44 +0700

    Initial commit

---
+++ SWD-BloodDonation/Program.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Đăng ký services
+
+
+
+var app = builder.Build();
+
+// Map controllers
+app.MapControllers();
+
+app.Run();
