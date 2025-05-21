d13db527f9052ae178e6fc767928cac2eb2df50b
Harry <thcstrandainghiathinh6a1@gmail.com>
2025-05-22 01:21:21 +0700

    add migration

--- SWD-BloodDonation/Program.cs
+++ SWD-BloodDonation/Program.cs
@@ -1,17 +1,22 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using SWD_BloodDonation.Models;
 
-
 var builder = WebApplication.CreateBuilder(args);
 
-// Đăng ký DbContext và sử dụng connection string trong appsettings.json
+// Đăng ký DbContext với Connection String từ appsettings.json hoặc Azure Configuration
 builder.Services.AddDbContext<BloodDonationContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-
 
 builder.Services.AddControllers();
 
 var app = builder.Build();
+
+// CHẠY MIGRATION trước khi app chạy
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<BloodDonationContext>();
+    db.Database.Migrate(); // Tự động tạo / cập nhật DB trên Azure
+}
 
 app.MapControllers();
 
