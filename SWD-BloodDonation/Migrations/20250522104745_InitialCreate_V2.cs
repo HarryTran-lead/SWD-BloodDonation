9a449f8210326f43eb064d95764c0d4e7c2c7a62
Harry <thcstrandainghiathinh6a1@gmail.com>
2025-05-22 17:59:28 +0700

    set migration in sql

--- SWD-BloodDonation/Migrations/20250522104745_InitialCreate_V2.cs
+++ SWD-BloodDonation/Migrations/20250522104745_InitialCreate_V2.cs
@@ -1,23 +1,22 @@
-﻿using System;
-using Microsoft.EntityFrameworkCore.Migrations;
+﻿using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
 
 namespace SWD_BloodDonation.Migrations
 {
     /// <inheritdoc />
-    public partial class InitialCreate : Migration
+    public partial class InitialCreate_V2 : Migration
     {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-           
+
         }
 
         /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-            
+
         }
     }
 }
